f2209b0205c9e9cdcb500c82332ac466699104c9
Alexander Bernauer <alex@copton.net>
2010-07-13 13:18:36 +0200

    case-study for LWB evaluation

---
+++ AkcConfig/src/StevesCastle.c
@@ -0,0 +1,5 @@
+void onHandle(int scanner, int tag) {
+	if (scanner == 9823 && tag == 9238)
+		play("danger.wav");
+}
+// Actifsource ID=[cff4438b-8e6f-11df-88d6-a50cae917c2c,73af5066-8e6e-11df-88d6-a50cae917c2c]
