a37044c2b547503e2f72e421a291532a9f9c26e3
Alexander Bernauer <alex@copton.net>
2010-07-13 15:15:27 +0200

    playing around with possible language extensions

--- AkcConfig/src/StevesCastle.c
+++ AkcConfig/src/StevesCastle.c
@@ -1,5 +1,5 @@
 void onHandle(int scanner, int tag) {
-	if (scanner == 9823 && tag == 9238)
-		play("danger.wav");
+	if (scanner == [] && tag == [])
+		play("[]");
 }
 // Actifsource ID=[cff4438b-8e6f-11df-88d6-a50cae917c2c,73af5066-8e6e-11df-88d6-a50cae917c2c]
